5679670d58264e5fa2c1567516c07fdca4330a2c
Furkan <Furkan@hhh.com>
2016-04-06 15:08:15 +0200

    A third small Fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld
              System.out.println("Hello World!");
 	     System.out.println("Hello World, again!");
 	     System.out.println("Hello World,for the thrid time!");
+	     System.out.println("What else should i do ?");
        }
 }
